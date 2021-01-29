7aee8970b5b9c2f6149b67ed3d570e39dc0f6c86
Kang <1073292238@qq.com>
2021-01-29 17:07:23 +0800

    更新首页样式

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,42 +5,55 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    postList: [
+      {
+        id: '1',
+        nickname: 'EdmundDKang',
+        avatarImg: 'https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJ8FLaHn52jz65uaDXl5od5of7iaJDDIRHTOuc7ibZwlS5I0saRTOH46nEwQ8xyvoMolwdMCDodRVEQ/132',
+        time: '12:33',
+        content: '哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈',
+        imgs: ['https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJ8FLaHn52jz65uaDXl5od5of7iaJDDIRHTOuc7ibZwlS5I0saRTOH46nEwQ8xyvoMolwdMCDodRVEQ/132','https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJ8FLaHn52jz65uaDXl5od5of7iaJDDIRHTOuc7ibZwlS5I0saRTOH46nEwQ8xyvoMolwdMCDodRVEQ/132','https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJ8FLaHn52jz65uaDXl5od5of7iaJDDIRHTOuc7ibZwlS5I0saRTOH46nEwQ8xyvoMolwdMCDodRVEQ/132','https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJ8FLaHn52jz65uaDXl5od5of7iaJDDIRHTOuc7ibZwlS5I0saRTOH46nEwQ8xyvoMolwdMCDodRVEQ/132'],
+        like: '2',
+        comment: '1',
+        star: ''
+      },
+      {
+        id: '2',
+        nickname: 'EdmundDKang',
+        avatarImg: 'https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJ8FLaHn52jz65uaDXl5od5of7iaJDDIRHTOuc7ibZwlS5I0saRTOH46nEwQ8xyvoMolwdMCDodRVEQ/132',
+        time: '12:33',
+        content: '埃德加啊大家艾菲奖阿覅你打的扩大看到复烤fog经济',
+        imgs: ['https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJ8FLaHn52jz65uaDXl5od5of7iaJDDIRHTOuc7ibZwlS5I0saRTOH46nEwQ8xyvoMolwdMCDodRVEQ/132'],
+        like: '',
+        comment: '1',
+        star: '3'
+      },
+      {
+        id: '3',
+        nickname: 'EdmundDKang',
+        avatarImg: 'https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJ8FLaHn52jz65uaDXl5od5of7iaJDDIRHTOuc7ibZwlS5I0saRTOH46nEwQ8xyvoMolwdMCDodRVEQ/132',
+        time: '12:33',
+        content: '埃德加啊大家艾菲奖阿覅你打的扩大看到复烤fog经济',
+        imgs: [],
+        like: '',
+        comment: '1',
+        star: '3'
+      },
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //this.getPostList()
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 获取论坛列表数据
    */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  async getPostList() {
+    // const ListData = await request('', {})
   },
 
   /**
